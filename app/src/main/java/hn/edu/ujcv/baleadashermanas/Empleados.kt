048c2e5089885078900c0893c9c92223615adad7
Carlos <cmchamorro99@hotmail.com>
2021-11-24 08:45:29 -0600

    Agregando pantalla de empleados y refactorizando campos de contraseñas (falta en empleados)

--- app/src/main/java/hn/edu/ujcv/baleadashermanas/Empleados.kt
+++ app/src/main/java/hn/edu/ujcv/baleadashermanas/Empleados.kt
@@ -3,18 +3,15 @@ package hn.edu.ujcv.baleadashermanas
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_acceso.*
+import kotlinx.android.synthetic.main.activity_empleados.*
 
-class Acceso : AppCompatActivity() {
-    var click = 0
+class Empleados : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_acceso)
-        btn_ingresar.setOnClickListener {
+        setContentView(R.layout.activity_empleados)
+        btn_menuPrincipalEmpleados.setOnClickListener {
             val intent = Intent(this, Principal::class.java)
             startActivity(intent)
         }
     }
-
-
 }
---
+++ app/src/main/java/hn/edu/ujcv/baleadashermanas/Principal.kt
@@ -0,0 +1,26 @@
+package hn.edu.ujcv.baleadashermanas
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_acceso.*
+import kotlinx.android.synthetic.main.activity_principal.*
+
+class Principal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal)
+        btn_empleados.setOnClickListener {
+            val intent = Intent(this, Empleados::class.java)
+            startActivity(intent)
+        }
+        imv_cerrarSesion.setOnClickListener {
+            val intent = Intent(this, Acceso::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+
+
+}

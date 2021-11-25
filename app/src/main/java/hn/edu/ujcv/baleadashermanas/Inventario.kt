7f2f5a4f4de067794bba39225adfd14d8163c6b5
Carlos <cmchamorro99@hotmail.com>
2021-11-25 07:02:23 -0600

    Agregando activity de inventario, ademas de las validaciones pendientes en las demas pantallas

--- app/src/main/java/hn/edu/ujcv/baleadashermanas/Inventario.kt
+++ app/src/main/java/hn/edu/ujcv/baleadashermanas/Inventario.kt
@@ -2,10 +2,26 @@ package hn.edu.ujcv.baleadashermanas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_inventario.*
+import java.util.ArrayList
 
 class Inventario : AppCompatActivity() {
+    var tipoMovimiento = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inventario)
+        llenarSpinner()
+
+    }
+
+    private fun llenarSpinner(){
+        val arrayAdapter: ArrayAdapter<*>
+        tipoMovimiento.add("Seleccione el tipo de movimiento")
+        tipoMovimiento.add("Ingreso")
+        tipoMovimiento.add("Retiro")
+        arrayAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,tipoMovimiento)
+        spi_tipoMovimiento.adapter = arrayAdapter
     }
 }
--- app/src/main/java/hn/edu/ujcv/baleadashermanas/Principal.kt
+++ app/src/main/java/hn/edu/ujcv/baleadashermanas/Principal.kt
@@ -3,9 +3,11 @@ package hn.edu.ujcv.baleadashermanas
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.NumberPicker
 import kotlinx.android.synthetic.main.activity_principal.*
 
 class Principal : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal)
@@ -16,6 +18,11 @@ class Principal : AppCompatActivity() {
 
         btn_clientes.setOnClickListener {
             val intent = Intent(this, Clientes::class.java)
+            startActivity(intent)
+        }
+
+        btn_inventario.setOnClickListener {
+            val intent = Intent(this, Inventario::class.java)
             startActivity(intent)
         }
 
